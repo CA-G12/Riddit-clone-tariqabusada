4a41023e8c1b5833bd0fc246cd00d783e12deb0b
tariqabusada <tariqesa101@gmail.com>
2022-09-07 21:12:15 +0200

    build: profile & home pages #18

--- server/database/config/ddl.sql
+++ server/database/config/ddl.sql
@@ -7,22 +7,23 @@ CREATE TABLE users (
     full_name VARCHAR(80) NOT NULL,
     email VARCHAR(80) NOT NULL UNIQUE,
     password VARCHAR(80) NOT NULL,
-    img_url VARCHAR(400) DEFAULT "https://i0.wp.com/researchictafrica.net/wp/wp-content/uploads/2016/10/default-profile-pic.jpg",
+    img_url VARCHAR(400) DEFAULT 'https://i0.wp.com/researchictafrica.net/wp/wp-content/uploads/2016/10/default-profile-pic.jpg',
     job VARCHAR(80) default null
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
+    post TEXT NOT NULL,
     date DATE NOT NULL,
-    like_count INT DEFAULT 0,
+    user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    date DATE NOT NULL,
+    comment TEXT NOT NULL,
+    user_id INT ,
+    post_id INT ,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (post_id) REFERENCES posts(id)
 );
